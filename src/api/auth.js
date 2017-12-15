ef57d352fe4ddce92538ef21f15019ce9406024d
Ozan Müyesseroğlu <ozanmuyes@gmail.com>
2017-12-16 01:40:20 +0300

    Add API support (WIP)

---
+++ src/api/auth.js
@@ -0,0 +1,7 @@
+// TODO Import the gateway instance
+
+// TODO The object (or the class) SHOULD contain these methods; \
+//      `login`, `logout` and `getUserInfo` where they make
+//      these API calls respectively; 'POST /api/token',
+//      'DELETE  /api/token' and 'GET /users/:userId'
+//      with access token in Authorization header.
---
+++ src/api/index.js
@@ -0,0 +1,8 @@
+// TODO Export the gateway instance from this
+
+// TODO Import axios
+
+// NOTE Do NOT all-import and then export, every file stands
+//      for itself. This means, for example, the API file
+//      for 'auth' MUST be imported like;
+//      `import authApi from 'api/auth'`
