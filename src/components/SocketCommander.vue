7786a7e4694a0f3a9e326075b141ff2ee7c38e1c
Ozan Müyesseroğlu <ozanmuyes@gmail.com>
2018-02-13 02:16:10 +0300

    WIP

--- src/components/SocketCommander.vue
+++ src/components/SocketCommander.vue
@@ -7,16 +7,30 @@
       ref="connectionForm"
     >
       <el-form-item label="URL">
-        <el-input v-model="connectionForm.url"></el-input>
+        <el-input
+          v-model="connectionForm.url"
+        ></el-input>
       </el-form-item>
 
-      <el-button
-        class="full-width"
-        @click="onConnectClick"
-        type="success"
-      >
-        Connect
-      </el-button>
+      <el-form-item>
+        <el-switch
+          active-color="#13ce66"
+          active-text="Auto Login"
+          inactive-color="#ff4949"
+          v-model="connectionForm.autoLogin"
+        >
+        </el-switch>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button
+          class="full-width"
+          @click="onConnectClick"
+          type="success"
+        >
+          Connect
+        </el-button>
+      </el-form-item>
     </el-form>
 
     <el-form
@@ -24,11 +38,9 @@
       label-width="120px"
       :model="commandForm"
       ref="commandForm"
-      @submit.prevent="console.log('Submit on command form prevented.')"
     >
       <el-form-item label="Command">
         <el-input
-          @keydown.enter.prevent="onSubmitClick"
           v-model="commandForm.command"
         ></el-input>
       </el-form-item>
@@ -36,7 +48,6 @@
       <el-form-item label="Arguments">
         <el-input
           class="arguments"
-          @keydown.enter.prevent="onSubmitClick"
           v-model="commandForm.args"
           :rows="1"
           type="textarea"
@@ -68,6 +79,8 @@
 </template>
 
 <script>
+  /* eslint-disable max-len */
+
   import io from 'socket.io-client';
 
   import { onConnectionErrored } from 'api';
@@ -81,13 +94,12 @@
         isLoggedIn: false,
         connectionForm: {
           url: 'http://localhost:3030',
+          autoLogin: true,
           //
         },
         commandForm: {
-          command: 'sck ls',
+          command: '',
           args: '',
-          // command: 'kick',
-          // args: '[1]',
           response: '',
         },
         //
@@ -122,55 +134,66 @@
 
           this.isConnected = true;
 
-          setTimeout((thisComponent) => {
-            /* eslint-disable no-param-reassign */
-            // TODO Login via user credentials ('access_token' in this app)
-            const access_token = store.state.user.access_token; // eslint-disable-line camelcase
+          // TODO Call `tryToLogin`
+          if (this.connectionForm.autoLogin) {
+            setTimeout((thisComponent) => {
+              this.tryToLogin();
+              // /* eslint-disable no-param-reassign */
+              // // TODO Login via user credentials ('access_token' in this app)
+              // const access_token = store.state.user.access_token; // eslint-disable-line camelcase
+              // // debugger;
+              // thisComponent.socket.emit('login', { access_token }, (response) => {
+              // // debugger;
+              //   if (response) {
+              //     if (response === true) {
+              //       thisComponent.isLoggedIn = true;
+              //       //
+              //     } else if (response.name && response.message) {
+              //       // If it's an error
+              // // debugger;
+              //       thisComponent.isLoggedIn = false;
+              //       //
+
+              //       // TODO Refresh the access token
+              //       onConnectionErrored({
+              //         // Synthetic error object
+              //         isSynthetic: true,
+              //         response: {
+              //           status: 403,
+              //           data: {
+              //             error: {
+              //               code: '13',
+              //               //
+              //             },
+              //             //
+              //           },
+              //           //
+              //         },
+              //         //
+              //       }, (newAccessToken) => { // TODO [WSMSTRFRSHTKNNDRTRYLGN-2]
+              //   console.log(`Got new access token: '${newAccessToken}'`);
+              //         // TODO Send login event again with new access token
+              //       });
+              //     }
+              //   } else {
+              //     // No return, nothing has changed
+              //     thisComponent.isLoggedIn = false;
+
+              //     //
+              //   }
+              // });
+
+              // //
+              // /* eslint-enable no-param-reassign */
+            }, 100, this);
+          } else {
+            // TODO One-off command upon login after page refresh
+//             this.socket.emit('sock ls', {}, (response) => {
 // debugger;
-            thisComponent.socket.emit('login', { access_token }, (response) => {
-debugger;
-              if (response) {
-                if (response === true) {
-                  thisComponent.isLoggedIn = true;
-                  //
-                } else if (response.name && response.message) {
-                  // If it's an error
-// debugger;
-                  thisComponent.isLoggedIn = false;
-                  //
-
-                  // TODO Refresh the access token
-                  onConnectionErrored({
-                    // Synthetic error object
-                    isSynthetic: true,
-                    response: {
-                      status: 403,
-                      data: {
-                        error: {
-                          code: '13',
-                          //
-                        },
-                        //
-                      },
-                      //
-                    },
-                    //
-                  }, (newAccessToken) => { // TODO [WSMSTRFRSHTKNNDRTRYLGN-2]
-console.log(`Got new access token: '${newAccessToken}'`);
-                    // TODO Send login event again with new access token
-                  });
-                }
-              } else {
-                // No return, nothing has changed
-                thisComponent.isLoggedIn = false;
-
-                //
-              }
-            });
-
-            //
-            /* eslint-enable no-param-reassign */
-          }, 100, this);
+//               console.log(response);
+//               //
+//             });
+          }
         });
 
         this.socket.on('disconnect', () => {
@@ -210,13 +233,15 @@ console.log(`Got new access token: '${newAccessToken}'`);
           //
         });
 
-        this.socket.emit('command', commandObj, (response) => {
+        this.socket.emit(command, args, (response) => {
+        // this.socket.emit('command', commandObj, (response) => {
           // console.log(`Command response: '${response}'`);
 
           let responseStr = '';
           if (typeof response === 'string') {
             responseStr = response;
           } else {
+// debugger;
             responseStr = JSON.parse(response);
           }
 
@@ -224,6 +249,56 @@ console.log(`Got new access token: '${newAccessToken}'`);
 // console.log(`\`response\` setting to: '${responseStr}'`);
           this.commandForm.response = responseStr;
         });
+      },
+      tryToLogin() {
+        /* eslint-disable no-param-reassign */
+        // TODO Login via user credentials ('access_token' in this app)
+        const access_token = store.state.user.access_token; // eslint-disable-line camelcase
+        // debugger;
+        this.socket.emit('login', { access_token }, (response) => {
+        // debugger;
+          if (response) {
+            if (response === true) {
+              this.isLoggedIn = true;
+              //
+            } else if (response.name && response.message) {
+              // If it's an error
+        // debugger;
+              this.isLoggedIn = false;
+              //
+
+              // TODO Refresh the access token
+              onConnectionErrored({
+                // Synthetic error object
+                isSynthetic: true,
+                response: {
+                  status: 403,
+                  data: {
+                    error: {
+                      code: '13',
+                      //
+                    },
+                    //
+                  },
+                  //
+                },
+                //
+              }, (newAccessToken) => { // TODO [WSMSTRFRSHTKNNDRTRYLGN-2]
+          console.log(`Got new access token: '${newAccessToken}'`);
+                return this.tryToLogin();
+                // TODO Send login event again with new access token
+              });
+            }
+          } else {
+            // No return, nothing has changed
+            this.isLoggedIn = false;
+
+            //
+          }
+        });
+
+        //
+        /* eslint-enable no-param-reassign */
       },
     },
     //
